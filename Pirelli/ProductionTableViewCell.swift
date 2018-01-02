7dbf0302525c7fbd140628656a76975e3de2f19d
jfaulkner25 <j.faulkner@me.com>
2018-01-02 21:49:29 +0000

    Second Commit

---
+++ Pirelli/ProductionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProductionTableViewCell.swift
+//  Pirelli
+//
+//  Created by James Faulkner on 02/01/2018.
+//  Copyright © 2018 The Hungry Gorilla. All rights reserved.
+//
+
+import UIKit
+
+class ProductionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
